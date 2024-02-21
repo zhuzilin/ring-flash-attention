e57b12765dae053804cbdc72f90f1c2e345c9ed3
Zilin Zhu <zilinzhu@tencent.com>
2024-02-21 18:27:05 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ring_flash_attn",
+    version="0.1",
+    author="zhuzilin",
+    url="https://github.com/zhuzilin/ring-flash-attention",
+    packages=find_packages(),
+)
