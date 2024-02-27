25a105f83e7ee7b21a9e6c326527a8f75051f413
Zilin Zhu <zilinzhu@tencent.com>
2024-02-27 18:11:57 +0800

    update varlen benchmark

--- benchmark/benchmark_varlen_qkvpacked_func.py
+++ benchmark/benchmark_varlen_qkvpacked_func.py
@@ -5,8 +5,7 @@ from ring_flash_attn import ring_flash_attn_varlen_qkvpacked_func
 import torch.cuda
 
 
-def benchmark_forward(f, num_benchmark_iter=1000, log=True):
-    torch.cuda.empty_cache()
+def benchmark(f, num_iter=100, forward_only=True, log=True):
     dtype = torch.bfloat16
     rank = dist.get_rank()
     world_size = dist.get_world_size()
@@ -26,6 +25,7 @@ def benchmark_forward(f, num_benchmark_iter=1000, log=True):
     qkv = torch.randn(
         seqlen, 3, nheads, d, device=device, dtype=dtype, requires_grad=True
     )
+    dout = torch.randn(seqlen, nheads, d, device=device, dtype=dtype)
 
     cu_seqlens_list = [
         torch.tensor([0, 8192], device=device, dtype=torch.int32),
@@ -41,10 +41,25 @@ def benchmark_forward(f, num_benchmark_iter=1000, log=True):
 
     begin = torch.cuda.Event(enable_timing=True)
     begin.record()
-    with torch.no_grad():
+    if forward_only:
+        with torch.no_grad():
 
-        for i in range(num_benchmark_iter):
-            _ = f(
+            for i in range(num_iter):
+                _ = f(
+                    qkv,
+                    cu_seqlens_list[i % len(cu_seqlens_list)],
+                    max_seqlen_list[i % len(max_seqlen_list)],
+                    dropout_p=dropout_p,
+                    causal=causal,
+                    window_size=(-1, -1),
+                    alibi_slopes=None,
+                    deterministic=deterministic,
+                    return_attn_probs=False,
+                )
+    else:
+        for i in range(num_iter):
+            qkv.grad = None
+            out = f(
                 qkv,
                 cu_seqlens_list[i % len(cu_seqlens_list)],
                 max_seqlen_list[i % len(max_seqlen_list)],
@@ -55,28 +70,28 @@ def benchmark_forward(f, num_benchmark_iter=1000, log=True):
                 deterministic=deterministic,
                 return_attn_probs=False,
             )
+            out.backward(dout)
     end = torch.cuda.Event(enable_timing=True)
     end.record()
     torch.cuda.synchronize(device=device)
     time = begin.elapsed_time(end) / 1000.0
 
     if rank == 0 and log:
-        print(f"{f.__name__} {num_benchmark_iter / time} iter/s, {time} sec")
+        print(f"{num_iter / time} iter/s, {time} sec")
 
 
 if __name__ == "__main__":
     dist.init_process_group("nccl")
     rank = dist.get_rank()
-    world_size = dist.get_world_size()
-    dtype = torch.bfloat16
-    device = torch.device(f"cuda:{rank}")
-    torch.cuda.set_device(device)
 
-    if rank == 0:
-        print("warmuping...")
-    benchmark_forward(flash_attn_varlen_qkvpacked_func, log=False)
-    benchmark_forward(ring_flash_attn_varlen_qkvpacked_func, log=False)
-    if rank == 0:
-        print("benchmark:")
-    benchmark_forward(flash_attn_varlen_qkvpacked_func)
-    benchmark_forward(ring_flash_attn_varlen_qkvpacked_func)
+    forward_only = False
+
+    for f in [
+        flash_attn_varlen_qkvpacked_func,
+        ring_flash_attn_varlen_qkvpacked_func,
+    ]:
+        torch.cuda.empty_cache()
+        if rank == 0:
+            print(f"# {f.__name__}")
+        benchmark(f, forward_only=forward_only, log=False)
+        benchmark(f, forward_only=forward_only, log=True)
